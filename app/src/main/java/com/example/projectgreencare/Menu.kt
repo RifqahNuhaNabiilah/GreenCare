60518c6a4b4c33ca66b7787f2c6501d3e862b144
RifqahNuhaNabiilah <rifqahnuhanabiila12@gmail.com>
2023-12-20 15:58:11 +0700

    firstcommit

--- app/src/main/java/com/example/projectgreencare/Menu.kt
+++ app/src/main/java/com/example/projectgreencare/Menu.kt
@@ -2,8 +2,13 @@ package com.example.projectgreencare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Menu : AppCompatActivity() {
+
+    lateinit var _rectangle_planttype: View
+    lateinit var _rectangle_plantcare: View
+    lateinit var _rectangle_aboutus: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
